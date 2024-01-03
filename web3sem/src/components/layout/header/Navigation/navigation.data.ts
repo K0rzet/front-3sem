d42c4781d7b6070260efc962322d5a57be507144
unknown <ehot2004@gmail.com>
2024-01-03 19:31:29 +0300

    added consts with links

--- web3sem/src/components/layout/header/Navigation/navigation.data.ts
+++ web3sem/src/components/layout/header/Navigation/navigation.data.ts
@@ -1,21 +1,22 @@
+import { ABOUT_URL, AUTH_URL, HOME_URL, TABLE_URL } from "../../../../utils/router-storage/router-constants";
 import { INavigation } from "./navigation.interface";
 
 export const navigationData: INavigation = {
   items: [
     {
-      link: "/",
+      link: HOME_URL,
       name: "Home",
     },
     {
-      link: "/table",
+      link: TABLE_URL,
       name: "Table",
     },
     {
-      link: "/about-us",
+      link: ABOUT_URL,
       name: "About Us",
     },
     {
-      link: "/register",
+      link: AUTH_URL,
       name: "Register",
     },
   ],
--- web3sem/src/utils/router-storage/router.data.ts
+++ web3sem/src/utils/router-storage/router.data.ts
@@ -3,28 +3,31 @@ import { IRouter } from "./router.interface";
 import MovieTable from "../../components/table/TestTable";
 import AboutUs from "../../components/AboutUs/AboutUs";
 import Auth from "../../components/auth/Auth";
+import { ABOUT_URL, AUTH_URL, HOME_URL, TABLE_URL } from "./router-constants";
 
 const Home = React.lazy(() => import("../../components/Home/Home"));
+
+
 
 export const routerData: IRouter = {
   items: [
     {
-      link: "/",
+      link: HOME_URL,
       name: "Home",
       component: Home,
     },
     {
-      link: "/table",
+      link: TABLE_URL,
       name: "Table",
       component: MovieTable,
     },
     {
-      link: "/about-us",
+      link: ABOUT_URL,
       name: "About Us",
       component: AboutUs,
     },
     {
-      link: "/register",
+      link: AUTH_URL,
       name: "register",
       component: Auth,
     },
